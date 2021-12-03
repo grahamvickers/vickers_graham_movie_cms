1de0adbec8d22f55407c0abd544d41d232a88c2f
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2021-12-03 12:41:11 -0500

    added greensock animations

--- back-end/public/js/main.js
+++ back-end/public/js/main.js
@@ -6,3 +6,22 @@ gsap.set(".pageTransition", {
     transformOrigin: 'left center',
     autoAlpha: 1
 });
+
+
+window.onload = function() {
+
+    var timeline = new TimelineMax();
+    timeline.from("#moviesContainer", 1, {y:100, autoAlpha: .5 }, 0)
+    timeline.from("#detailsCon", 1, {y:50, autoAlpha: .5}, 0)
+    timeline.from("#landing", 1, {y:100, autoAlpha: .5}, 0)
+    timeline.from("#create", 1, {y:100, autoAlpha: .5}, 0)
+}
+
+
+function clicked(e)
+{
+    console.log("confirm")
+    if(!confirm('Are you sure you want to remove this movie?')) {
+        e.preventDefault();
+    }
+}
